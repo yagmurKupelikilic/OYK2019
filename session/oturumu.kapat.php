690cadc9440f0aca20b45818a5abe4d973799ba4
yagmurKupelikilic <yagmurkupelikilic@gmail.com>
2019-07-25 12:35:40 +0300

    session konusu tamamlandı

--- session/oturumu.kapat.php
+++ session/oturumu.kapat.php
@@ -2,5 +2,5 @@
 @session_start();
 $_SESSION["giris_basarili"] = 0;
 //session_destroy();
-echo "Çıkış yaptınız...";
+echo "Çıkış yaptınız... <br /> <a href='giris.kontrol.php'>Yeniden girmek için tıklayınız.</a>";
 ?>
