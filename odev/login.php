723cb7c95e1d44752a240613ab2141a28e9d2742
yagmurKupelikilic <yagmurkupelikilic@gmail.com>
2019-07-26 14:07:36 +0300

    login page yapıldı

---
+++ odev/login.php
@@ -0,0 +1,34 @@
+<?php
+@session_start();
+if($_SESSION["giris_basarili"] == 1){
+  header("location: index.php");
+  die();
+}
+
+//giriş başarılıysa
+if(isset($_POST["adi"])){
+  if($_POST["adi"] == "ahmet" and $_POST["pass"] == "123"){
+    header("location: index.php");
+    $_SESSION["giris_basarili"] = 1;
+    $_SESSION["yetki"]= 1;
+    die();
+  }
+  if($_POST["adi"] == "mehmet" and $_POST["pass"] == "789"){
+    header("location: index.php");
+    $_SESSION["giris_basarili"] = 1;
+    $_SESSION["yetki"]= 2;
+    die();
+  }
+echo "tekrar deneyiniz...";
+}
+
+ ?>
+
+
+<form  method="post">
+  Adi: <input type="text" name="adi" value="">
+  <br />  <br />
+  Password: <input type="text" name="pass" value="">
+  <br />  <br />
+  <input type="submit" value="Giriş Yap" />
+</form>
