a9339c51e251923d572e5a9a5e1a52432cb53f35
yagmurKupelikilic <yagmurkupelikilic@gmail.com>
2019-07-26 11:21:02 +0300

    iki sayıyı database' ekleme yapıldı

---
+++ deneme/db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username   = "root";
+$password   = "123";
+$dbname     = "bilgiler";
+// Veritabanı bağlantısının oluşturulması
+$db = mysqli_connect($servername, $username, $password, $dbname);
+// Varsa, bağlantı hatasının ekrana yazdırılarak programın sonlandırılması
+if (!$db) { die("Hata oluştu: " . mysqli_connect_error()); }
+//echo "Bağlantı tamam!";
+?>
---
+++ deneme/islemler.php
@@ -0,0 +1,30 @@
+<h1>iki sayıyı database'e gönderme</h1>
+<?php
+include("db.php");
+if (isset( $_POST["sayi1"] )) {  // Form POST edilmiş...
+  // Önce EKLEME için SQL hazırlayalım...
+  $SQL = sprintf("
+      INSERT INTO
+        deneme
+      SET
+        sayi1  = '%s',
+        sayi2  = '%s'",
+  $_POST["sayi1"], $_POST["sayi2"]);
+  // SQL komutunu MySQL veritabanı üzerinde çalıştır!
+  $rows  = mysqli_query($db, $SQL);
+  echo "<h4>Kayıt başarıyla eklendi</h4>";
+}  // Form POST edilmiş...
+?>
+
+
+<form method="post">
+  1.Sayıyı giriniz:
+  <input type="text" name="sayi1" placeholder="sayi giriniz">
+  <br><br>
+  2.Sayiyi giriniz:
+  <input type="text" name="sayi2"  placeholder="sayi giriniz">
+  <br><br>
+
+  <input type="submit" value="Gönder">
+
+</form>
